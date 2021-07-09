09ec617e9ea86627ace1ca1cdb89d3986b7f43a4
alws34 <34398061+alws34@users.noreply.github.com>
2021-07-09 21:27:30 +0300

    remove Bixby .bat file

---
+++ remove_bixby.bat
@@ -0,0 +1,36 @@
+cd /d "D:\Google Drive\Alon\ADB"
+adb devices
+pause
+@echo BYE BYE BIXBY
+adb shell pm uninstall -k --user 0 com.samsung.android.bixby.agent
+
+adb shell pm uninstall -k --user 0 com.samsung.android.bixby.es.globalaction
+
+adb shell pm uninstall -k --user 0 com.samsung.android.bixbyvision.framework
+
+adb shell pm uninstall -k --user 0 com.samsung.android.bixby.wakeup
+
+adb shell pm uninstall -k --user 0 com.samsung.android.bixby.plmsync
+
+adb shell pm uninstall -k --user 0 com.samsung.android.bixby.voiceinput
+
+adb shell pm uninstall -k --user 0 com.samsung.systemui.bixby
+
+adb shell pm uninstall -k --user 0 com.samsung.android.bixby.agent.dummy
+
+adb shell pm uninstall -k --user 0 com.samsung.android.app.settings.bixby
+
+adb shell pm uninstall -k --user 0 com.samsung.systemui.bixby2
+
+adb shell pm uninstall -k --user 0 com.samsung.android.bixby.service
+
+adb shell pm uninstall -k --user 0 com.samsung.android.app.routines
+
+adb shell pm uninstall -k --user 0 com.samsung.android.visionintelligence
+
+adb shell pm uninstall -k --user 0 com.samsung.android.app.spage
+
+pause
+
+@echo rebooting device...
+adb reboot
